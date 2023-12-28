3b8dcc373412dcf49bf9b9077acc450ccf9f3a71
katie-guite <kguite@gmail.com>
2023-12-27 20:35:03 -0800

    initial commit

---
+++ src/generator.js
@@ -0,0 +1,205 @@
+// Code that generators the random lorem ipsum text
+
+// Create a new object called loremIpsum by invoking the GenerateNewText constructor function
+export const loremIpsum = new GenerateNewText();
+
+// Constructor function that creates an object with the words property
+function GenerateNewText() {
+  // Add property to the object
+  this.sentences =
+    [
+    "Jolene, Jolene, Jolene, Jolene",
+    "I'm begging of you please don't take my man", 
+    "Jolene, Jolene, Jolene, Jolene",
+    "Please don't take him just because you can",
+    "Your beauty is beyond compare",
+    "With flaming locks of auburn hair",
+    "With ivory skin and eyes of emerald green",
+    "Your smile is like a breath of spring",
+    "Your voice is soft like summer rain",
+    "And I cannot compete with you, Jolene",
+    "He talks about you in his sleep",
+    "There's nothing I can do to keep",
+    "From crying when he calls your name, Jolene",
+    "And I can easily understand",
+    "How you could easily take my man",
+    "But you don't know what he means to me, Jolene",
+    "Jolene, Jolene, Jolene, Jolene",
+    "I'm begging of you please don't take my man",
+    "Jolene, Jolene, Jolene, Jolene",
+    "Please don't take him just because you can",
+    "You could have your choice of men",
+    "But I could never love again",
+    "He's the only one for me, Jolene",
+    "I had to have this talk with you",
+    "My happiness depends on you",
+    "And whatever you decide to do, Jolene",
+    "Jolene, Jolene, Jolene, Jolene",
+    "I'm begging of you please don't take my man",
+    "Jolene, Jolene, Jolene, Jolene",
+    "Please don't take him even though you can",
+    "Jolene, Jolene",
+    "I grew up poor and ragged, just a simple country girl.",
+    "I wanted to be pretty more than anything in the world,",
+    "Like Barbie or the models in the Fredricks' catalog.",
+    "From rags to wishes in my dreams I could have it all.",
+    "I'm just a backwoods Barbie, too much makeup, too much hair.",
+    "Don't be fooled by thinkin' that the goods are not all there.",
+    "Don't let these false eyelashes lead you to believe that",
+    "I'm as shallow as I look 'cause I run true and deep.",
+    "I've always been misunderstood because of how I look.",
+    "Don't judge me by the cover 'cause I'm a real good book.",
+    "So read into it what you will, but see me as I am.",
+    "The way I look is just a country girl's idea of glam.",
+    "I'm just a backwoods Barbie in a push-up bra and heels.",
+    "I might look artificial, but where it counts I'm real.",
+    "And I'm all dolled up and hopin' for a chance to prove my worth,",
+    "And even backwoods Barbie's get their feelings hurt.",
+    "I'm just a backwoods Barbie, too much makeup, too much hair.",
+    "Don't be fooled by thinkin' that the goods are not all there.",
+    "Yes, I can see where I could be misjudged upon first glance;",
+    "But even backwoods Barbie's deserve a second chance.",
+    "I'm just a backwoods Barbie just asking for a chance,",
+    "Just a backwoods Barbie.",
+    "New york city ain't no kind of place",
+    "For a country girl with a friendly face",
+    "If you smile people look at you funny",
+    "They take it wrong",
+    "The greenest state in the land of the free",
+    "And the home of the grand ole opry",
+    "Is calling me back to my smoky mountain home",
+    "I wish I had my old fishin' pole",
+    "And was sitting on the banks of the fishing hole",
+    "Eating green apples and waiting for the fish to bite",
+    "Life ain't as simple as it used to be",
+    "Since the big apple took a bite out of me",
+    "And lord, I'm so tennessee homesick that I could die",
+    "But I ain't been home in I don't know when",
+    "If I had it all to do over again",
+    "Tonight I'd sleep in my old feather bed",
+    "What I wouldn't give for a little bitty taste",
+    "Of mama's homemade chocolate cake",
+    "Tennessee homesick blues running through my head",
+    "Mama you can fluff my feather bed",
+    "Just as soon as I can I'm gonna head",
+    "Back to the tennessee hills and it better be soon",
+    "Daddy you can load the rifles up",
+    "We're gonna load them dogs on the pickup truck",
+    "And take off to calhoun country and catch us a coon",
+    "Eatin' grits and gravy and country ham",
+    "Go to church on sunday with dinner on the grounds",
+    "Tennessee homesick blues are runnin' through my head",
+    "Good lord have mercy on a country girl",
+    "Tryin' to make a living in a rhinestone world",
+    "It's hard to be a diamond in a rhinestone world",
+    "With tennessee homesick blues runnin' through my head",
+    "I've got those tennessee homesick blues runnin' through my head",
+    "Tennessee homesick blues",
+    "Tumble outta bed",
+    "And I stumble to the kitchen",
+    "Pour myself a cup of ambition",
+    "And yawn and stretch",'
+    "And try to come to life",
+    "Jump in the shower",
+    "And the blood starts pumpin'",
+    "Out on the street",
+    "The traffic starts jumpin'",
+    "The folks like me on the job from 9 to 5",
+    "Workin' 9 to 5,",
+    "What a way to make a livin'",
+    "Barely gettin' by",
+    "It's all takin' and no givin'",
+    "They just use your mind",
+    "And they never give you credit",
+    "It's enough to drive you crazy",
+    "If you let it",
+    "9 to 5, for service and devotion",
+    "You would think that I",
+    "Would deserve a fair promotion",
+    "Want to move ahead",
+    "But the boss won't seem to let me",
+    "I swear sometimes that man is out to get me",
+    "They let you dream",
+    "Just to watch 'em shatter",
+    "You're just a step",
+    "On the boss-man's ladder",
+    "But you got dreams",
+    "He'll never take away",
+    "You're in the same boat",
+    "With a lot of your friends",
+    "Waitin' for the day your ship'll come in",
+    "'N' the tide's gonna turn",
+    "And it's all gonna roll your way",
+    "Workin' 9 to 5",
+    "What a way to make a livin'",
+    "Barely gettin' by",
+    "It's all takin' and no givin'",
+    "They just use your mind",
+    "And you never get the credit",
+    "It's enough to drive you crazy",
+    "If you let it",
+    "9 to 5, yeah
+    "They got you where they want you",
+    "There's a better life",
+    "And you think about it, don't you?",
+    "It's a rich man's game",
+    "No matter what they call it",
+    "And you spend your life",
+    "Puttin' money in his wallet",
+    "9 to 5, whoa",
+    "What a way to make a livin'",
+    "Barely gettin' by",
+    "It's all takin' and no givin'",
+    "They just use your mind",
+    "And they never give you credit",
+    "It's enough to drive you crazy",
+    "If you let it",
+    "9 to 5, yeah",
+    "They got you where they want you",
+    "There's a better life",
+    "And you dream about it, don't you?",
+    "It's a rich man's game",
+    "No matter what they call it",
+    "And you spend your life",
+    "Puttin' money in his wallet",
+    "Working 9 to 5",
+   ];
+}
+
+// Method to the GenerateNewText constructor function that generates a random sentence
+GenerateNewText.prototype.getRandomSentence = function() {
+  let randomSentence = this.sentences[Math.floor(Math.random() * this.sentences.length)];
+	return randomSentence;
+};
+
+// Method to the GenerateNewText constructor function that generates a paragraph from random sentences
+GenerateNewText.prototype.getParagraph = function() {
+  let paragraph = '';
+  // Set the minimum number of words
+  let minimumCharacterLength = 250;
+  let firstSentence = true;
+  while (paragraph.length < minimumCharacterLength) {
+    if (firstSentence) {
+      paragraph = paragraph.concat(this.getRandomSentence());
+      firstSentence = false;
+    } else {
+      paragraph = paragraph.concat(' ' + this.getRandomSentence());
+    }
+  }
+  return paragraph;
+};
+
+// Method to the GenerateNewText constructor function that gerates multiple paragraphs from paragraphs
+GenerateNewText.prototype.getAllParagraphs = function(numberOfParagraphs) {
+  let allParagraphs = [];
+  // Generate the number of paragraphs as specified by the user
+  while (allParagraphs.length < numberOfParagraphs) {
+    allParagraphs.push(this.getParagraph());
+  }
+  // Convert array into HTML string
+  let paragraphHTML = '';
+  allParagraphs.forEach(function (paragraph) {
+    paragraphHTML += '<p>' + paragraph + '</p>';
+  });
+  return paragraphHTML;
+};
